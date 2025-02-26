e86c829fc314fbb55e0b6eb20c2f90f9d00f0615
Samma3ll <skinny666@gmail.com>
2025-02-26 20:01:26 +0100

    ch3 L3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from player import Player
+from player import Player, Bullet
 from asteroid import Asteroid
 from asteroidfied import AsteroidField
 
@@ -10,10 +10,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Bullet.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     AsteroidField()
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED
 
 
 class Player(CircleShape):
@@ -38,7 +38,28 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        bullet = Bullet(self.position.x, self.position.y)
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        bullet.velocity = forward * PLAYER_SHOOT_SPEED
+        return bullet
+
+
+class Bullet(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+        self.radius = SHOT_RADIUS
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), (int(
+            self.position.x), int(self.position.y)), self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
